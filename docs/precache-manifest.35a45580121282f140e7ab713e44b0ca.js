3d4ef3d749eba27d1085ad754a429901c0debd0e
mariaoca <mariaolidcamarena@gmail.com>
2019-01-25 12:58:08 +0100

    create docs

---
+++ docs/precache-manifest.35a45580121282f140e7ab713e44b0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2ebc0a4a08b575108a77",
+    "url": "./static/js/main.2ebc0a4a.chunk.js"
+  },
+  {
+    "revision": "75dceaf7d4aa0ee98f02",
+    "url": "./static/js/1.75dceaf7.chunk.js"
+  },
+  {
+    "revision": "2ebc0a4a08b575108a77",
+    "url": "./static/css/main.83847ba8.chunk.css"
+  },
+  {
+    "revision": "42eb4bf392d741345afa3b70b607411e",
+    "url": "./index.html"
+  }
+];
